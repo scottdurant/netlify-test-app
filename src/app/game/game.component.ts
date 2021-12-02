61dbd135683196ac24271f72a64b3264aa379a83
scottdurant <scott.durant.16@cnu.edu>
2021-12-01 19:02:40 -0500

    added game page and routing

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css'],
+})
+export class GameComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
