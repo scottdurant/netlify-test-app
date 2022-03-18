40bf67181cd5ef0bbc2500e67ca8fbe18f5b1658
scottdurant <scott.durant.16@cnu.edu>
2022-03-17 21:03:29 -0400

    getting back 10 quotes from api

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 import { QuoteService } from '../quote.service';
 import { Quote } from '../quote';
+
+enum Characters {
+  Elaine = 'ELAINE',
+  George = 'GEORGE',
+  Jerry = 'JERRY',
+  Kramer = 'KRAMER',
+}
 
 @Component({
   selector: 'game',
@@ -11,7 +16,6 @@ import { Quote } from '../quote';
 })
 export class GameComponent implements OnInit {
   quotes: Quote[] = [];
-  headers: string[] = [];
 
   constructor(private quoteService: QuoteService) {}
 
@@ -20,18 +24,18 @@ export class GameComponent implements OnInit {
   }
 
   getQuotes(): void {
-    // this.quoteService.getQuotes().subscribe((resp) => {
-    //   const keys = resp.headers.keys();
-    //   this.headers = keys.map((key) => `${key}: ${resp.headers.get(key)}`);
-
-    //   this.quotes = { ...resp.body! };
-    //   console.log(this.quotes);
-    // });
-
-    this.quoteService.getQuotes().subscribe((data) => {
+    this.quoteService.getRandomQuote().subscribe((data) => {
       console.log(data);
-      this.quotes = JSON.parse(JSON.stringify(data.body));
-      console.log(this.quotes);
+      let allQuotes = data.body?.quotes;
+      if (allQuotes) {
+        while (this.quotes.length < 10) {
+          let randomQuote = allQuotes[Math.floor(Math.random() * 422)];
+          if (Object.keys(Characters).includes(randomQuote.author)) {
+            this.quotes.push(randomQuote);
+          }
+          console.log(this.quotes);
+        }
+      }
     });
   }
 }
--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Quote } from './quote';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuoteService {
-  quotesUrl: string = 'https://seinfeld-quotes.herokuapp.com/quotes';
+  private quotesUrl: string = 'https://seinfeld-quotes.herokuapp.com/quotes';
+
   constructor(private http: HttpClient) {}
 
-  getQuotes(): Observable<HttpResponse<Quote[]>> {
-    return this.http.get<Quote[]>(this.quotesUrl, {
+  getRandomQuote(): Observable<HttpResponse<{ quotes: Quote[] }>> {
+    return this.http.get<{ quotes: Quote[] }>(this.quotesUrl, {
       observe: 'response',
       responseType: 'json',
     });
