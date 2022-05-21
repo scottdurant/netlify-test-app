10c3f849318402a3af64d676cd901b777c2b9159
scottdurant <scott.durant.16@cnu.edu>
2022-05-21 12:04:20 -0400

    added error handling to quote service

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { Quote } from './quote';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +17,26 @@ export class QuoteService {
   constructor(private http: HttpClient) {}
 
   getAllQuotes(): Observable<{ quotes: Quote[] }> {
-    return this.http.get<{ quotes: Quote[] }>(this.quotesUrl);
+    return this.http
+      .get<{ quotes: Quote[] }>(this.quotesUrl)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
   }
 }
